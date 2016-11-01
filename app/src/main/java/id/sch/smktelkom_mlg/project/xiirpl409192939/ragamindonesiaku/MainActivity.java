687e1be886fcf5547237d4994879c1d7d1bb68df
BertiDila <bertidila@gmail.com>
2016-11-01 07:06:19 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl409192939/ragamindonesiaku/MainActivity.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom_mlg.project.xiirpl409192939.ragamindonesiaku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
