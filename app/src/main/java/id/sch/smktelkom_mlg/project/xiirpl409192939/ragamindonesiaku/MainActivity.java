e807f45f93e8b97b5e60996d794ea278a9014907
NanaShin <krisnakumalasari29@gmail.com>
2016-11-17 19:39:04 +0700

    Card View

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl409192939/ragamindonesiaku/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl409192939/ragamindonesiaku/MainActivity.java
@@ -1,14 +1,55 @@
 package id.sch.smktelkom_mlg.project.xiirpl409192939.ragamindonesiaku;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.project.xiirpl409192939.ragamindonesiaku.adapter.ProvinsiAdapter;
+import id.sch.smktelkom_mlg.project.xiirpl409192939.ragamindonesiaku.model.Provinsi;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Provinsi> mList = new ArrayList<>();
+    ProvinsiAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new ProvinsiAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+    }
+
+    private void fillData()
+    {
+        Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.provinsi);
+        String[] arDeskripsi = resources.getStringArray(R.array.detail_provinsi);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+
+        for (int i = 0; i < arFoto.length; i++) {
+
+            arFoto[i] = a.getDrawable(i);
+        }
+        a.recycle();
+
+        for (int i = 0; i < arJudul.length; i++) {
+            mList.add(new Provinsi(arJudul[i], arDeskripsi[i], arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl409192939/ragamindonesiaku/model/Provinsi.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.project.xiirpl409192939.ragamindonesiaku.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by NanaShin on 17/11/2016.
+ */
+
+public class Provinsi {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Provinsi(String judul, String deskripsi, Drawable foto)
+    {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
